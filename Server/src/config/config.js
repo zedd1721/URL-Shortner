02460e20975e59645ab76b2bd3c9397179f69e08
Zain Shaikh <szain2024@gmail.com>
2025-06-25 21:28:27 +0530

    Cookie update

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOption ={
     httpOnly: false,
     secure: process.env.NODE_ENV === "production",
-    sameSite:"lax",
+    sameSite:"None",
     maxAge: 1000 *60 *60 *24 *7, //1 week
 }
